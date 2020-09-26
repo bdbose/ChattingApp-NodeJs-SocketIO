0bd60b7e40ddfbb2b36844688a8a24507ef4a7c7
bdbose <bdbose123@gmail.com>
2020-09-26 19:33:23 +0530

    Fix

--- index.js
+++ index.js
@@ -14,32 +14,48 @@ app.use(cors());
 
 const port = process.env.PORT || '4000';
 
+const Time = () => {
+	return new Date().toLocaleTimeString();
+};
+
+let client = 0;
+
 io.on('connection', (socket) => {
 	console.log('connect');
+	client++;
 	socket.on('join', ({ name, room }, callback) => {
-		console.log(name, room);
 		socket.emit('message', {
 			user: 'Admin',
 			message: `${name},welcome to ${room}`,
+			time: Time(),
+			count: client,
 		});
-		socket.broadcast
-			.to(room)
-			.emit('message', { user: 'admin', message: `${name} just joined.` });
+
+		socket.broadcast.to(room).emit('message', {
+			user: 'Admin',
+			message: `${name} just joined.`,
+			time: Time(),
+		});
 		socket.join(room);
 		callback();
 	});
-
 	socket.on('sendMessage', (data) => {
-		io.to('global').emit('message', { user: data.name, message: data.message });
+		io.to('global').emit('message', {
+			user: data.name,
+			message: data.message,
+			time: Time(),
+		});
 		console.log(data);
 	});
-
 	socket.on('disconnect', (data) => {
-		socket.emit('message', { user: 'Admin', message: `${data.name} left` });
-		console.log('discount');
+		client--;
+		socket.on('dis', (data) => {
+			console.log(data);
+		});
+		console.log('discount', data);
 	});
 });
 
 http.listen(port, () => {
-	console.log('SERVER IS UP');
+	console.log('SERVER IS UP at', port);
 });
